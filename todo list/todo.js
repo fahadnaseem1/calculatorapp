882f249c07985f9d67048f3cc1887caebfc15191
Fahad Naseem <siddiquefahad638@gmail.com>
2026-08-23 18:45:06 +0530

    adding functionality

--- todo list/todo.js
+++ todo list/todo.js
@@ -1,23 +1,26 @@
   let todoList=[];
   displayElement();
 
-
-
  function addTodo(){
     let inputElement = document.querySelector('.input');
+    let dateElement = document.querySelector('.date');
+    let dateItem = dateElement.value;
     let todoItem= inputElement.value;
-    todoList.push(todoItem);
+    todoList.push({item: todoItem, date: dateItem});
     inputElement.value='';
-    displayElement();
+    dateElement.value='';
+    displayElement(); 
  } 
 
   function displayElement(){
     let containerElement = document.querySelector('.todo-container');
     let newHtml='';
     for( let i =0;i< todoList.length;i++){
+      let {item , date} = todoList[i];
         newHtml+= `
         <div>
-            <span>${todoList[i]} </span>
+            <span>${item} </span>
+            <span>${date}</span>
             <button onClick="todoList.splice(${i},1); displayElement();">delete</button>
         </div>
         `; 
